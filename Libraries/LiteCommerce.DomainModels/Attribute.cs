d3c3e435f6334689a070efafd4d9e7b9965cc060
vietvite <vanviet8367@gmail.com>
2020-06-19 01:34:43 +0700

    Remove Attribute table

--- Libraries/LiteCommerce.DomainModels/Attribute.cs
+++
@@ -1,9 +0,0 @@
-namespace LiteCommerce.DomainModels
-{
-    public class Attribute
-    {
-        public int AttributeID { get; set; }
-        public int CategoryID { get; set; }
-        public string AttributeName { get; set; }
-    }
-}
