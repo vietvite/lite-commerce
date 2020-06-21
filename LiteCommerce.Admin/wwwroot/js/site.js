c53bf833d3137f6b57fe7ef395acae861401e112
vietvite <vanviet8367@gmail.com>
2020-06-21 23:00:55 +0700

    Add static files

--- LiteCommerce.Admin/wwwroot/js/site.js
+++ LiteCommerce.Admin/wwwroot/js/site.js
@@ -3,16 +3,21 @@
 
 // Write your JavaScript code.
 $(document).ready(function () {
-    $("#avatar").click(function() {
+    $("#avatar").click(function () {
         $("input[id='PhotoPath_file']").click();
     });
-    
-    $("form#delete-selected").submit(function(e) {
+
+    $("form#delete-selected").submit(function (e) {
         let ok = confirm("Không thể hoàn tác sau khi xóa. Bạn chắc chắn muốn xóa?")
-        if(!ok) e.preventDefault()
+        if (!ok) e.preventDefault()
     });
-    
-    $("#checkAll").click(function(){
+
+    $("#checkAll").click(function () {
         $('input:checkbox').not(this).prop('checked', this.checked);
     });
+
+    $("a#delete-attribute").click(function (e) {
+        let ok = confirm("Không thể hoàn tác sau khi xóa. Bạn chắc chắn muốn xóa?")
+        if (!ok) e.preventDefault()
+    })
 });
