e64419ba9cde744fb357b68ae79d0e4315e03365
vietvite <vanviet8367@gmail.com>
2020-06-22 10:14:37 +0700

    Change notify UI post field error

--- LiteCommerce.Admin/wwwroot/js/site.js
+++ LiteCommerce.Admin/wwwroot/js/site.js
@@ -3,21 +3,31 @@
 
 // Write your JavaScript code.
 $(document).ready(function () {
+    // Click in image to show up upload image window
     $("#avatar").click(function () {
         $("input[id='PhotoPath_file']").click();
     });
 
+    // Confirm delete item in list of each View index
     $("form#delete-selected").submit(function (e) {
         let ok = confirm("Không thể hoàn tác sau khi xóa. Bạn chắc chắn muốn xóa?")
         if (!ok) e.preventDefault()
     });
 
+    // Select all item in list of each View index
     $("#checkAll").click(function () {
         $('input:checkbox').not(this).prop('checked', this.checked);
     });
 
+    // Delete product attribute
     $("a#delete-attribute").click(function (e) {
         let ok = confirm("Không thể hoàn tác sau khi xóa. Bạn chắc chắn muốn xóa?")
         if (!ok) e.preventDefault()
     })
+
+    // Append has-error class to parent div if form has error message
+    if ($(".field-validation-error").length) {
+        $(".field-validation-error.help-block").parent().addClass("has-error")
+    }
+
 });
