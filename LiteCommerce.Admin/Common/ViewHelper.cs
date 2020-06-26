b5f730d84d9018f7879df6ce85ce370cbd7cc15d
vietvite <vanviet8367@gmail.com>
2020-06-26 17:12:34 +0700

    Fix paginate button lack of optional query

--- LiteCommerce.Admin/Common/ViewHelper.cs
+++ LiteCommerce.Admin/Common/ViewHelper.cs
@@ -35,23 +35,26 @@ namespace LiteCommerce.Common
 
 
         #region Paginate button
-        public static Func<string, int, string, string, IHtmlContent> RenderButton =
-            (ControllerName, Page, SearchValue, DisplayContent) =>
-                new HtmlContentBuilder().AppendHtml($"<li><a href = '{ControllerName}?{JustPageQuery(Page)}{MaybeSearchQuery(SearchValue)}'>{DisplayContent}</a></li>");
+        // public static Func<string, int, string, string, IHtmlContent> RenderButton =
+        //     (ControllerName, Page, SearchValue, DisplayContent) =>
+        //         new HtmlContentBuilder().AppendHtml($"<li><a href = '{ControllerName}?{JustPageQuery(Page)}{MaybeSearchQuery(SearchValue)}'>{DisplayContent}</a></li>");
+        public static Func<string, int, string, string, string, IHtmlContent> RenderButton =
+            (ControllerName, Page, DisplayContent, SearchValue, OptionalQuery) =>
+                new HtmlContentBuilder().AppendHtml($"<li><a href = '{ControllerName}?{JustPageQuery(Page)}{MaybeSearchQuery(SearchValue)}{OptionalQuery}'>{DisplayContent}</a></li>");
 
-        public static IHtmlContent RenderPageButtonToRight(string ControllerName, int CurrentPage, int Width, string SearchValue)
+        public static IHtmlContent RenderPageButtonToRight(string ControllerName, int CurrentPage, int Width, string SearchValue, string OptionalQuery)
         {
             var html = new HtmlContentBuilder();
             for (int i = CurrentPage; i <= Width; i++)
-                html.AppendHtml(RenderButton(ControllerName, i, SearchValue, Convert.ToString(i)));
+                html.AppendHtml(RenderButton(ControllerName, i, Convert.ToString(i), SearchValue, OptionalQuery));
 
             return html;
         }
-        public static IHtmlContent RenderPageButtonToLeft(string ControllerName, int CurrentPage, int Width, string SearchValue)
+        public static IHtmlContent RenderPageButtonToLeft(string ControllerName, int CurrentPage, int Width, string SearchValue, string OptionalQuery)
         {
             var html = new HtmlContentBuilder();
             for (int i = Width; i <= CurrentPage; i++)
-                html.AppendHtml(RenderButton(ControllerName, i, SearchValue, Convert.ToString(i)));
+                html.AppendHtml(RenderButton(ControllerName, i, Convert.ToString(i), SearchValue, OptionalQuery));
 
             return html;
         }
@@ -61,7 +64,7 @@ namespace LiteCommerce.Common
         //         ListPageNumber.Aggregate(new HtmlContentBuilder(), (acc, iter) =>
         //             (HtmlContentBuilder)acc.AppendHtml(RenderButton(ControllerName, iter, SearchValue, Convert.ToString(iter))));
 
-        public static IHtmlContent RenderPageButton(string ControllerName, int CurrentPage, int MaxPage, int Width, string SearchValue)
+        public static IHtmlContent RenderPageButton(string ControllerName, int CurrentPage, int MaxPage, int Width, string SearchValue, string OptionalQuery = "")
         {
             var html = new HtmlContentBuilder();
 
@@ -73,34 +76,36 @@ namespace LiteCommerce.Common
             int MaxWidthRight = (CurrentPage + Width) < MaxPage
                 ? CurrentPage + Width : MaxPage;
 
+            OptionalQuery = OptionalQuery ?? "";
+
             // | « |...|
             if (MaxWidthLeft > 1)
             {
-                html.AppendHtml(RenderButton(ControllerName, 1, SearchValue, "«"));
+                html.AppendHtml(RenderButton(ControllerName, 1, "«", SearchValue, OptionalQuery));
                 html.AppendHtml("<li><a>...</a></li>");
             }
 
             // Page is first page => | 1 | 2 | 3 | 4 |...| » |
             if (CurrentPage == 1)
-                html.AppendHtml(RenderPageButtonToRight(ControllerName, CurrentPage, MaxWidthRight, SearchValue));
+                html.AppendHtml(RenderPageButtonToRight(ControllerName, CurrentPage, MaxWidthRight, SearchValue, OptionalQuery));
 
             // Page is last page => |n-4|n-3|n-2|n-1| n |
             else if (CurrentPage == MaxPage)
-                html.AppendHtml(RenderPageButtonToLeft(ControllerName, CurrentPage, MaxWidthLeft, SearchValue));
+                html.AppendHtml(RenderPageButtonToLeft(ControllerName, CurrentPage, MaxWidthLeft, SearchValue, OptionalQuery));
 
             // Page is between first and last page => |n-3|n-2|n-1| n |n+1|n+2|n+3|
             else
             {
-                html.AppendHtml(RenderPageButtonToLeft(ControllerName, CurrentPage - 1, MaxWidthLeft, SearchValue));
+                html.AppendHtml(RenderPageButtonToLeft(ControllerName, CurrentPage - 1, MaxWidthLeft, SearchValue, OptionalQuery));
                 html.AppendHtml($"<li class='active'><a>{CurrentPage}</a></li>");
-                html.AppendHtml(RenderPageButtonToRight(ControllerName, CurrentPage + 1, MaxWidthRight, SearchValue));
+                html.AppendHtml(RenderPageButtonToRight(ControllerName, CurrentPage + 1, MaxWidthRight, SearchValue, OptionalQuery));
             }
 
             // |...| » |
             if (MaxWidthRight < MaxPage)
             {
                 html.AppendHtml("<li><a>...</a></li>");
-                html.AppendHtml(RenderButton(ControllerName, MaxPage, SearchValue, "»"));
+                html.AppendHtml(RenderButton(ControllerName, MaxPage, "»", SearchValue, OptionalQuery));
             }
 
             return html;
